585771f8050f123d808e8b7bcdf6d39891cfea62
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-25 14:36:06 +0800

    chore: update snap

--- test/dts.test.ts
+++ test/dts.test.ts
@@ -31,18 +31,18 @@ test('dts', () => {
   expect(generateTypeDecarations())
     .toMatchInlineSnapshot(`
       "declare global {
-          const customDefault: typeof import('default')['default']
-          const foobar: typeof import('foobar')['foobar']
-          const ref: typeof import('vue')['ref']
-          const reactive: typeof import('vue')['reactive']
-          const computed: typeof import('vue')['computed']
-          const toRefs: typeof import('vue')['toRefs']
-          const THREE: typeof import('three')
-          const useState: typeof import('react')['useState']
-          const useEffect: typeof import('react')['useEffect']
-          const useRef: typeof import('react')['useRef']
-          const \$: typeof import('jquery')['\$']
+        const customDefault: typeof import('default')['default']
+        const foobar: typeof import('foobar')['foobar']
+        const ref: typeof import('vue')['ref']
+        const reactive: typeof import('vue')['reactive']
+        const computed: typeof import('vue')['computed']
+        const toRefs: typeof import('vue')['toRefs']
+        const THREE: typeof import('three')
+        const useState: typeof import('react')['useState']
+        const useEffect: typeof import('react')['useEffect']
+        const useRef: typeof import('react')['useRef']
+        const \$: typeof import('jquery')['\$']
       }
       export default {}"
-  `)
+    `)
 })
