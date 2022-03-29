e37fe6fce190884f73a8d9224a7862fd790210a0
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-29 19:29:54 +0800

    chore: setup playground

---
+++ playground/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
