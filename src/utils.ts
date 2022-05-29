7dc2daf3219b7dfd020860779be0366c4acf27e3
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-29 10:20:55 +0800

    Update src/utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -18,7 +18,7 @@ export const separatorRE = /[,[\]{}\n]/g
 export const matchRE = /(?<![\w_$/)]\.)([\w_$]+)\s*(?:[.()[\];+*&|`<>,\n-])/g
 
 const regexRE = /\/.*?(?<!\\)(?<!\[[^\]]*)\/[gimsuy]*/g
-const multilineCommentsRE = /\/\*(.|\s)*?\*\//gms
+const multilineCommentsRE = /\/\*.*?\*\//gms
 const singlelineCommentsRE = /\/\/.*$/gm
 const templateLiteralRE = /\$\{(.*?)\}/g
 const quotesRE = [
