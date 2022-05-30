3f83a7d34dfcaa7b49c80e657a23836f2de5ca0d
Leo <leonid.buneev@tirecheck.cz>
2022-05-30 22:52:00 +0200

    Drop files from gitignore

--- dist/addons.d.ts
+++
@@ -1,6 +0,0 @@
-import { i as Addon } from './types-40a2cd16.js';
-import 'magic-string';
-
-declare const vueTemplateAddon: () => Addon;
-
-export { vueTemplateAddon };
--- dist/addons.mjs
+++
@@ -1,3 +0,0 @@
-export { v as vueTemplateAddon } from './chunks/vue-template.mjs';
-import 'mlly';
-import 'magic-string';
