741015b9cd4fc45c1ca75cf05ef3c8e6aa39dd69
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-23 04:29:23 +0800

    feat(vue-template)!: auto unwrap (#72)

--- test/vue-template.test.ts
+++ test/vue-template.test.ts
@@ -1,20 +1,26 @@
 import { describe, expect, test } from 'vitest'
+import { compileTemplate } from 'vue/compiler-sfc'
 import { createUnimport } from '../src'
 
-const input = `
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return _openBlock(), _createElementBlock("div", null, [
-    _createElementVNode("h1", null, _toDisplayString($setup.count) + " x " + _toDisplayString(_ctx.multiplier) + " = " + _toDisplayString($setup.doubled), 1),
-    _toDisplayString(_ctx.foo),
-    _createElementVNode("button", { onClick: $setup.inc }, " Inc ")
-  ]);
-}
-`
+const result = compileTemplate({
+  id: 'template.vue',
+  filename: 'template.vue',
+  source: `
+    <div>{{ foo }}</div>
+    <div>{{ foo + 1 }}</div>
+    <div v-if="foo"></div>
+    <div v-if="foo === 1"></div>
+    <div @click="foo"></div>
+  `,
+  compilerOptions: {
+    hoistStatic: false
+  }
+})
 
 describe('vue-template', () => {
   const ctx = createUnimport({
     imports: [
-      { name: 'multiplier', from: 'foo', as: 'multiplier' }
+      { name: 'foo', from: 'foo', as: 'foo' }
     ],
     addons: {
       vueTemplate: true
@@ -22,16 +28,44 @@ describe('vue-template', () => {
   })
 
   test('inject', async () => {
-    expect((await ctx.injectImports(input, 'a.vue')).code.toString()).toMatchInlineSnapshot(`
-      "import { multiplier as __unimport_multiplier } from 'foo';
-      function _sfc_render(_ctx, _cache, \$props, \$setup, \$data, \$options) {
-        return _openBlock(), _createElementBlock(\\"div\\", null, [
-          _createElementVNode(\\"h1\\", null, _toDisplayString(\$setup.count) + \\" x \\" + _toDisplayString(__unimport_multiplier) + \\" = \\" + _toDisplayString(\$setup.doubled), 1),
-          _toDisplayString(_ctx.foo),
-          _createElementVNode(\\"button\\", { onClick: \$setup.inc }, \\" Inc \\")
-        ]);
-      }
-      "
+    expect(result.code).toMatchInlineSnapshot(`
+      "import { toDisplayString as _toDisplayString, createElementVNode as _createElementVNode, openBlock as _openBlock, createElementBlock as _createElementBlock, createCommentVNode as _createCommentVNode, Fragment as _Fragment } from \\"vue\\"
+
+      export function render(_ctx, _cache) {
+        return (_openBlock(), _createElementBlock(_Fragment, null, [
+          _createElementVNode(\\"div\\", null, _toDisplayString(_ctx.foo), 1 /* TEXT */),
+          _createElementVNode(\\"div\\", null, _toDisplayString(_ctx.foo + 1), 1 /* TEXT */),
+          (_ctx.foo)
+            ? (_openBlock(), _createElementBlock(\\"div\\", { key: 0 }))
+            : _createCommentVNode(\\"v-if\\", true),
+          (_ctx.foo === 1)
+            ? (_openBlock(), _createElementBlock(\\"div\\", { key: 1 }))
+            : _createCommentVNode(\\"v-if\\", true),
+          _createElementVNode(\\"div\\", {
+            onClick: _cache[0] || (_cache[0] = (...args) => (_ctx.foo && _ctx.foo(...args)))
+          })
+        ], 64 /* STABLE_FRAGMENT */))
+      }"
+    `)
+    expect((await ctx.injectImports(result.code, 'a.vue')).code.toString()).toMatchInlineSnapshot(`
+      "import { foo as _unimport_foo } from 'foo';
+      import { unref as _unimport_unref_ } from 'vue';import { toDisplayString as _toDisplayString, createElementVNode as _createElementVNode, openBlock as _openBlock, createElementBlock as _createElementBlock, createCommentVNode as _createCommentVNode, Fragment as _Fragment } from \\"vue\\"
+
+      export function render(_ctx, _cache) {
+        return (_openBlock(), _createElementBlock(_Fragment, null, [
+          _createElementVNode(\\"div\\", null, _toDisplayString(_unimport_unref_(_unimport_foo)), 1 /* TEXT */),
+          _createElementVNode(\\"div\\", null, _toDisplayString(_unimport_unref_(_unimport_foo) + 1), 1 /* TEXT */),
+          (_unimport_unref_(_unimport_foo))
+            ? (_openBlock(), _createElementBlock(\\"div\\", { key: 0 }))
+            : _createCommentVNode(\\"v-if\\", true),
+          (_unimport_unref_(_unimport_foo) === 1)
+            ? (_openBlock(), _createElementBlock(\\"div\\", { key: 1 }))
+            : _createCommentVNode(\\"v-if\\", true),
+          _createElementVNode(\\"div\\", {
+            onClick: _cache[0] || (_cache[0] = (...args) => (_unimport_unref_(_unimport_foo) && _unimport_unref_(_unimport_foo)(...args)))
+          })
+        ], 64 /* STABLE_FRAGMENT */))
+      }"
     `)
   })
 
@@ -39,12 +73,13 @@ describe('vue-template', () => {
     expect(ctx.generateTypeDecarations()).toMatchInlineSnapshot(`
       "export {}
       declare global {
-        const multiplier: typeof import('foo')['multiplier']
+        const foo: typeof import('foo')['foo']
       }
       // for vue template auto import
+      import { UnwrapRef } from 'vue'
       declare module 'vue' {
         interface ComponentCustomProperties {
-          multiplier: typeof import('foo')['multiplier']
+          readonly foo: UnwrapRef<typeof import('foo')['foo']>
         }
       }
       "
