dcb5ca1c3093dc38080481312fa9e6aa6a0870aa
Anthony Fu <anthonyfu117@hotmail.com>
2022-10-24 16:41:08 +0800

    chore: update coverage

--- test/global.test.ts
+++ test/global.test.ts
@@ -5,7 +5,8 @@ describe('global', () => {
   it('basic', async () => {
     const unimport = createUnimport({
       imports: [
-        { name: 'default', from: 'jquery', as: '$' }
+        { name: 'default', from: 'jquery', as: '$' },
+        { name: 'foo', from: 'foo', as: 'disabled', disabled: true }
       ],
       presets: [
         {
@@ -30,5 +31,10 @@ describe('global', () => {
     expect(fakeGlobal.ref(1).value).toBe(1)
     expect(fakeGlobal.computed(() => 1 + 2).value).toBe(3)
     expect(typeof fakeGlobal.$).toBe('function')
+
+    const fakeGlobal2: any = {}
+    await installGlobalAutoImports(await unimport.getImports(), { globalObject: fakeGlobal2 })
+    expect(Object.keys(fakeGlobal2).sort())
+      .toEqual(Object.keys(fakeGlobal).sort())
   })
 })
