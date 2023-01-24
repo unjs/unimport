c910b8c612f9719649c26beb4a257868906e8f26
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-24 11:07:28 +0100

    test: stable root path in snapshot

--- test/dts.test.ts
+++ test/dts.test.ts
@@ -3,6 +3,7 @@ import { expect, test } from 'vitest'
 import { createUnimport } from '../src'
 
 test('dts', async () => {
+  const cwd = process.cwd()
   const { generateTypeDeclarations, init } = createUnimport({
     imports: [
       { name: 'default', from: 'default', as: 'customDefault' },
@@ -30,31 +31,31 @@ test('dts', async () => {
       './playground/composables/**'
     ],
     dirsScanOptions: {
-      cwd: process.cwd()
+      cwd
     }
   })
 
   await init()
 
-  expect(await generateTypeDeclarations())
+  expect((await generateTypeDeclarations()).replaceAll(cwd, '<root>'))
     .toMatchInlineSnapshot(`
       "export {}
       declare global {
         const $: typeof import('jquery')['$']
         const THREE: typeof import('three')
-        const bump: typeof import('/Users/antfu/i/unimport/playground/composables/index')['bump']
+        const bump: typeof import('<root>/playground/composables/index')['bump']
         const computed: typeof import('vue')['computed']
         const customDefault: typeof import('default')['default']
-        const foo: typeof import('/Users/antfu/i/unimport/playground/composables/foo')['default']
+        const foo: typeof import('<root>/playground/composables/foo')['default']
         const foobar: typeof import('foobar')['foobar']
-        const localA: typeof import('/Users/antfu/i/unimport/playground/composables/index')['localA']
-        const localBAlias: typeof import('/Users/antfu/i/unimport/playground/composables/index')['localBAlias']
-        const multiplier: typeof import('/Users/antfu/i/unimport/playground/composables/index')['multiplier']
-        const nested: typeof import('/Users/antfu/i/unimport/playground/composables/nested/index')['default']
+        const localA: typeof import('<root>/playground/composables/index')['localA']
+        const localBAlias: typeof import('<root>/playground/composables/index')['localBAlias']
+        const multiplier: typeof import('<root>/playground/composables/index')['multiplier']
+        const nested: typeof import('<root>/playground/composables/nested/index')['default']
         const reactive: typeof import('vue')['reactive']
         const ref: typeof import('vue')['ref']
         const toRefs: typeof import('vue')['toRefs']
-        const useDoubled: typeof import('/Users/antfu/i/unimport/playground/composables/index')['useDoubled']
+        const useDoubled: typeof import('<root>/playground/composables/index')['useDoubled']
         const useEffect: typeof import('react')['useEffect']
         const useRef: typeof import('react')['useRef']
         const useState: typeof import('react')['useState']
