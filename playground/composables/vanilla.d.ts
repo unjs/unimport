aa7cb685611f7069c92d73b36a7452ae28c116e6
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-18 13:59:00 +0800

    fix: better `.d.ts` handling on dirs scan

---
+++ playground/composables/vanilla.d.ts
@@ -0,0 +1,9 @@
+export const vanillaA: 1 | 2 | 3
+
+export interface VanillaInterface {
+  foo: string
+}
+
+export function vanillaTypeOnlyFunction(): boolean
+
+export type { VanillaInterface as VanillaInterfaceAlias }
