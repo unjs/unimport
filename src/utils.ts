4ed29d28c22d7546351ac0d9cf06060b957554d3
Michael Brevard <yonshi29@gmail.com>
2024-01-22 23:09:04 +0200

    Update the map

--- src/utils.ts
+++ src/utils.ts
@@ -5,34 +5,7 @@ import MagicString from 'magic-string'
 import type { Import, InlinePreset, MagicStringResult, TypeDeclarationOptions } from './types'
 import { stripCommentsAndStrings } from './regexp'
 import fs from "fs"
-export const files={}
-export const excludeRE = [
-  // imported/exported from other module
-  /\b(import|export)\b([\s\w_$*{},]+)\sfrom\b/gs,
-  // defined as function
-  /\bfunction\s*([\w_$]+?)\s*\(/gs,
-  // defined as class
-  /\bclass\s*([\w_$]+?)\s*{/gs,
-  // defined as local variable
-  /\b(?:const|let|var)\s+?(\[.*?\]|\{.*?\}|.+?)\s*?[=;\n]/gs,
-]
-
-export const importAsRE = /^.*\sas\s+/
-export const separatorRE = /[,[\]{}\n]|\bimport\b/g
-
-/**
- *                                                                            |       |
- *                    destructing   case&ternary    non-call     inheritance   |  id   |
- *                         ↓             ↓             ↓             ↓         |       |
- */
-export const matchRE = /(^|\.\.\.|(?:\bcase|\?)\s+|[^\w_$\/)]|(?:\bextends)\s+)([\w_$]+)\s*(?=[.()[\]}}:;?+\-*&|`<>,\n]|\b(?:instanceof|in)\b|$|(?<=extends\s+\w+)\s+{)/g
-
-const regexRE = /\/[^\s]*?(?<!\\)(?<!\[[^\]]*)\/[gimsuy]*/g
-
-export function stripCommentsAndStrings(code: string, options?: StripLiteralOptions) {
-  return stripLiteral(code, options)
-    .replace(regexRE, 'new RegExp("")')
-}
+const files = new Map()
 
 export function defineUnimportPreset(preset: InlinePreset): InlinePreset {
   return preset
