da1733b7ef0fe7504c8c530a13f4e0912ac306c8
Tamago <40710111+zekunjin@users.noreply.github.com>
2024-02-06 18:09:27 +0800

    fix: insert imports after shebang (#324)
    * fix: insert imports after shebang err
    
    * chore: rm unnecessary conditional statements

--- src/utils.ts
+++ src/utils.ts
@@ -237,6 +237,11 @@ export function addImportToCode(
     return _staticImports
   }
 
+  function hasShebang() {
+    const shebangRegex = /^#!.+/
+    return shebangRegex.test(s.original)
+  }
+
   if (mergeExisting && !isCJS) {
     const existingImports = findStaticImportsLazy()
     const map = new Map<StaticImport, Import[]>()
@@ -273,10 +278,12 @@ export function addImportToCode(
       ? findStaticImportsLazy().reverse().find(i => i.end <= firstOccurrence)?.end ?? 0
       : 0
 
-    if (insertionIndex === 0)
-      s.prepend(`${newEntries}\n`)
-    else
+    if (insertionIndex > 0)
       s.appendRight(insertionIndex, `\n${newEntries}\n`)
+    else if (hasShebang())
+      s.appendLeft(s.original.indexOf('\n') + 1, `\n${newEntries}\n`)
+    else
+      s.prepend(`${newEntries}\n`)
   }
 
   return {
