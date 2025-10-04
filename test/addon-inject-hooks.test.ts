4b47ceff67b859fea004404619c6342620656516
KazariEX <zinkawaii@qq.com>
2025-10-04 16:44:01 +0800

    test: update snapshot

--- test/addon-inject-hooks.test.ts
+++ test/addon-inject-hooks.test.ts
@@ -43,11 +43,11 @@ describe('addon inject hooks', () => {
       .toMatchInlineSnapshot(`
         "export {}
         declare global {
-          const computed: typeof import('vue')['computed']
-          const ref: typeof import('vue')['ref']
-          const useEffect: typeof import('react')['useEffect']
-          const useState: typeof import('react')['useState']
-          const watch: typeof import('vue')['watch']
+          const computed: typeof import('vue').computed
+          const ref: typeof import('vue').ref
+          const useEffect: typeof import('react').useEffect
+          const useState: typeof import('react').useState
+          const watch: typeof import('vue').watch
         }"
       `)
   })
